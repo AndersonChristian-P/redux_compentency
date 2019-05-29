31d9fca2bd8b2fbe3aa9b10e76fd13a4bb796b0b
Christian Anderson <ca96187@gmail.com>
2019-05-29 16:31:20 -0600

    created ctrl code to pull data from db

--- server/ProductCtrl.js
+++ server/ProductCtrl.js
@@ -1,5 +1,12 @@
 module.exports = {
   getProduct: async (req, res) => {
     const db = req.app.get("db")
+
+    try {
+      const data = await db.getMensProducts()
+      res.status(200).send(data)
+    } catch (err) {
+      res.sendStatus(404)
+    }
   }
 }
